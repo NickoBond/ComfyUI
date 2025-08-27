af86caed13b60a735f7cbd77a087496a156f5749
NickoBond <32357542+NickoBond@users.noreply.github.com>
2025-08-27 16:43:44 +0300

    Update wan22.sh

--- wan22.sh
+++ wan22.sh
@@ -48,7 +48,7 @@ ESRGAN_MODELS=(
 CONTROLNET_MODELS=(
 )
 
-DIFFUSION_MODELS(
+DIFFUSION_MODELS=(
     "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
     "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
 )
