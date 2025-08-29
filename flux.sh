943d91c7635d19d81d9def5ced1971f540f44b36
NickoBond <32357542+NickoBond@users.noreply.github.com>
2025-08-29 14:53:06 +0300

    Update flux.sh

--- flux.sh
+++ flux.sh
@@ -18,6 +18,8 @@ NODES=(
     "https://github.com/aidenli/ComfyUI_NYJY"
     "https://github.com/changwook987/ComfyUI-Small-Utility"
     "https://github.com/crystian/ComfyUI-Crystools"
+    "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    "https://github.com/kijai/ComfyUI-DepthAnythingV2"
     "https://github.com/OgreLemonSoup/ComfyUI-Load-Image-Gallery"
     "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
     "https://github.com/rgthree/rgthree-comfy"
