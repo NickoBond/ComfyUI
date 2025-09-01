565635463672f5e0dfa31d36e0b02543906c168a
NickoBond <32357542+NickoBond@users.noreply.github.com>
2025-09-01 14:33:10 +0300

    Update wan22.sh

--- wan22.sh
+++ wan22.sh
@@ -96,6 +96,9 @@ function provisioning_start() {
     provisioning_get_files \
         "${COMFYUI_DIR}/models/text_encoders" \
         "${TEXTENCODER_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/diffusers" \
+        "${DIFFUSERS_MODELS[@]}"
     provisioning_print_end
 }
 
